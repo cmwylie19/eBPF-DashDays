1514f9230e2c06b5c2f5fd95cd0b2e32624a5347
Case Wylie <cmwylie19@defenseunicorns.com>
2024-01-26 13:48:15 -0500

    update
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
+++ eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
@@ -2,16 +2,10 @@
 #![no_main]
 use aya_bpf::{bpf_printk, helpers::bpf_get_current_comm};
 use aya_bpf::helpers::bpf_get_current_uid_gid;
-use aya_bpf::helpers::bpf_ktime_get_ns;
-use aya_bpf::helpers::bpf_probe_read_kernel_str_bytes;
-use aya_bpf::helpers::bpf_probe_read_user_str_bytes;
-use aya_bpf::BpfContext;
 use aya_bpf::{
     helpers::bpf_probe_read_user, macros::tracepoint, maps::HashMap, programs::TracePointContext,
 };
 use aya_log_ebpf::info;
-use core::convert::TryInto;
-use core::ffi::c_char;
 use file_controller_2_common::FileLog;
 
 #[aya_bpf::macros::map]
@@ -27,8 +21,7 @@ pub fn file_controller_2(ctx: TracePointContext) -> u32 {
 
 fn try_file_controller_2(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "tracepoint sys_enter_openat called");
-    let mut dest = [0u8; 16];
-    let mut buf = [0u8; 16];
+
 
     let uid = bpf_get_current_uid_gid() as u64;
     info!(&ctx, "uid: {}", uid);
