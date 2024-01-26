671b8ce465195fd10374a05352022dc7ed1e864d
Case Wylie <cmwylie19@defenseunicorns.com>
2024-01-26 16:36:11 -0500

    update
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- file-controller-3/file-controller-3-ebpf/src/main.rs
+++ file-controller-3/file-controller-3-ebpf/src/main.rs
@@ -4,12 +4,12 @@
 use aya_bpf::helpers::*;
 use aya_bpf::macros::*;
 
-use aya_bpf::{macros::kprobe, programs::ProbeContext};
-use aya_log_ebpf::info;
-use file_controller_3_common::Filename;
 use aya_bpf::maps::HashMap;
 use aya_bpf::BpfContext;
 use aya_bpf::PtRegs;
+use aya_bpf::{macros::kprobe, programs::ProbeContext};
+use aya_log_ebpf::info;
+use file_controller_3_common::Filename;
 
 #[map(name = "PIDS")]
 static mut PIDS: HashMap<u32, Filename> = HashMap::with_max_entries(10240000, 0);
@@ -24,23 +24,24 @@ pub fn file_controller_3(ctx: ProbeContext) -> u32 {
 
 fn try_file_controller_3(ctx: ProbeContext) -> Result<u32, u32> {
     info!(&ctx, "function syscalls:sys_enter_execv called");
+    unsafe {
+        let pid = ctx.pid();
+        //  info!(&ctx, "args: {} ",ctx.arg(0).unwrap());
 
-     let pid = ctx.pid();
-    //  info!(&ctx, "args: {} ",ctx.arg(0).unwrap());
+        if PIDS.get(&pid).is_none() {
+            let regs = PtRegs::new(ctx.arg(0).unwrap());
+            let filename_addr: *const u8 = regs.arg(0).unwrap();
 
-    if PIDS.get(&pid).is_none() {
-        let regs = PtRegs::new(ctx.arg(0).unwrap());
-        let filename_addr: *const u8 = regs.arg(0).unwrap();
+            let mut buf = [0u8; 127];
+            let filename_len = bpf_probe_read_user_str(filename_addr as *const u8, &mut buf)
+                .map_err(|e| e as u32)? as u8;
 
-        let mut buf = [0u8; 127];
-        let filename_len = bpf_probe_read_user_str(filename_addr as *const u8, &mut buf)
-            .map_err(|e| e as u32)? as u8;
-
-        let log_entry = Filename {
-            filename: buf,
-            filename_len,
-        };
-        PIDS.insert(&pid, &log_entry, 0).unwrap();
+            let log_entry = Filename {
+                filename: buf,
+                filename_len,
+            };
+            PIDS.insert(&pid, &log_entry, 0).unwrap();
+        }
     }
     Ok(0)
 }
