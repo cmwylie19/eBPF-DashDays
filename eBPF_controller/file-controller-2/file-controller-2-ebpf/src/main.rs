973aa5a6416107a959729d9e6f5835af4306a78e
Case Wylie <cmwylie19@defenseunicorns.com>
2024-01-26 13:59:46 -0500

    update
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
+++ eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
@@ -1,19 +1,12 @@
 #![no_std]
 #![no_main]
 use aya_bpf::{bpf_printk, helpers::bpf_get_current_comm};
-use aya_bpf::helpers::bpf_get_current_uid_gid;
+use aya_bpf::helpers::*;
 use aya_bpf::{
-    macros::{map, tracepoint, xdp}, 
-    helpers::bpf_probe_read_user,  maps::HashMap, programs::TracePointContext,
+    helpers::bpf_probe_read_user, macros::tracepoint, maps::HashMap, programs::TracePointContext,
 };
 use aya_log_ebpf::info;
 use file_controller_2_common::FileLog;
-#[map(name = "EXECVE_EVENTS")]
-static mut EXECVE_EVENTS: PerfEventArray<ExecveCalls> = PerfEventArray::<ExecveCalls>::with_max_entries(1024, 0);
-
-#[map(name = "IP_RECORDS")]
-static mut IP_RECORDS: PerfEventArray<IpRecord> = PerfEventArray::<IpRecord>::with_max_entries(1024, 0);
-
 
 #[aya_bpf::macros::map]
 static EVENTS: HashMap<u64, FileLog> = HashMap::with_max_entries(1024, 0);
@@ -25,51 +18,27 @@ pub fn file_controller_2(ctx: TracePointContext) -> u32 {
         Err(ret) => ret,
     }
 }
-unsafe fn try_syspection(ctx: &TracePointContext) -> Result<i32, i64> {
 
-    // Get the process that is executing the command
-    let mut exec_buf = [0u8; 42];
-    let exec = bpf_get_current_comm().unwrap_or_default();
-
-    // Get the command that is being executed and stores it in exec_comm
-    let exec_comm = ctx.read_at::<*const u8>(16)?;
-    _ = bpf_probe_read_user_str_bytes(exec_comm, &mut exec_buf);
-
-    // Create a buffer for the arguments of the command
-    let mut arg_buf = [[0u8; ARG_SIZE]; ARG_COUNT];
-    
-    // Get the arguments of the command
-    let argv = ctx.read_at::<*const *const u8>(24)?;
-    for i in 0..ARG_COUNT {
-        let arg_ptr = bpf_probe_read_user(argv.offset(i as isize))?;
-
-        if arg_ptr.is_null() {
-            break;
-        }
-
-        bpf_probe_read_user_str_bytes(arg_ptr, &mut arg_buf[i as usize]).unwrap_or_default();
-    }
-
-    let execve_calls = ExecveCalls {
-        caller: exec,
-        command: exec_buf,
-        args: arg_buf,
-    };
-
-    EXECVE_EVENTS.output(ctx, &execve_calls, 0);
-
-    info!(
-        ctx, "curr_comm: {}, exec_comm: {}", from_utf8_unchecked(&execve_calls.caller), from_utf8_unchecked(&execve_calls.command)
-    );
-
-    Ok(0)
-}
 fn try_file_controller_2(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "tracepoint sys_enter_openat called");
 
 
     let uid = bpf_get_current_uid_gid() as u64;
     info!(&ctx, "uid: {}", uid);
+
+    let tsig = ctx.read_at::<u64>(24).unwrap() as u32;
+    if tsig == 0 { return Ok(0); }
+    let tpid = ctx.read_at::<i64>(16).unwrap() as i32;
+    let pid = (bpf_get_current_pid_tgid() >> 32).try_into().unwrap();
+    let tid = bpf_get_current_pid_tgid() as u32;
+    let comm = bpf_get_current_comm().unwrap();
+    info!(&ctx, "comm: {}", comm);
+    info!(
+        &ctx,
+        "tracepoint sys_enter_openat called: pid: {}, tid: {}, tpid: {}, tsig: {}",
+        pid, tid, tpid, tsig
+    );
+  //  let fname_ptr: usize = ctx.read_at(24).unwrap();
     // unsafe {
     //     let comm = bpf_get_current_comm().unwrap();
     //     let fname_ptr: usize = ctx.read_at(24).unwrap();
@@ -79,11 +48,7 @@ fn try_file_controller_2(ctx: TracePointContext) -> Result<u32, u32> {
     //         fname_ptr
     //     );
     // }
-
-    match unsafe {try_syspection(&ctx)} {
-        Ok(ret) => 0,
-        Err(ret) => ret as i32,
-    }
+    
 
     // let mut dest = [0u8; 256];
 
