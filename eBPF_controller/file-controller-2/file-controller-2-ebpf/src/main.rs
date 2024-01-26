2f799c3022f5e4fd73189d77fe955f5bb42797d2
Case Wylie <cmwylie19@defenseunicorns.com>
2024-01-26 12:56:46 -0500

    update
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
+++ eBPF_controller/file-controller-2/file-controller-2-ebpf/src/main.rs
@@ -25,13 +25,13 @@ pub fn file_controller_2(ctx: TracePointContext) -> u32 {
 
 fn try_file_controller_2(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "tracepoint sys_enter_openat called");
-
+    let mut dest = [0u8; 256];
     // // Get the current user ID
     let uid = bpf_get_current_uid_gid() as u64;
     info!(&ctx, "uid: {}", uid);
 
     let user = unsafe {
-        bpf_probe_read_user_str_bytes(ctx.as_ptr() as *const u8, 256);
+        bpf_probe_read_user_str_bytes(ctx.as_ptr() as *const u8, &mut [u8]);
     };
     info!(&ctx, "user ",user );
     // let mut dest = [0u8; 256];
