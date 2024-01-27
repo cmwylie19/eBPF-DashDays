fb9e3d8247c465271b85c06df74c06ddee55a298
Case Wylie <cmwylie19@defenseunicorns.com>
2024-01-27 08:30:05 -0500

    update
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- tracepoint-controller/tracepoint-controller-ebpf/src/main.rs
+++ tracepoint-controller/tracepoint-controller-ebpf/src/main.rs
@@ -2,18 +2,11 @@
 #![no_main]
 
 use aya_bpf::{
-    
     cty::c_long,
-    helpers::{
-        bpf_get_current_pid_tgid,
-        bpf_probe_read_user_str_bytes,
-    },
+    helpers::{bpf_get_current_pid_tgid, bpf_probe_read_user_str_bytes},
 };
 
-use aya_bpf::{
-    macros::tracepoint,
-    programs::TracePointContext,
-};
+use aya_bpf::{macros::tracepoint, programs::TracePointContext};
 use aya_log_ebpf::info;
 
 #[tracepoint]
@@ -24,32 +17,29 @@ pub fn tracepoint_controller(ctx: TracePointContext) -> c_long {
     }
 }
 
-fn try_tracepoint_controller(ctx: TracePointContext) -> Result<c_long, c_long>  {
+fn try_tracepoint_controller(ctx: TracePointContext) -> Result<c_long, c_long> {
     info!(&ctx, "tracepoint sys_enter_execve called");
 
     const FILENAME_OFFSET: usize = 16;
-    let filename_addr: u64 =
-        unsafe { ctx.read_at(FILENAME_OFFSET)? };
+    let filename_addr: u64 = unsafe { ctx.read_at(FILENAME_OFFSET)? };
 
     const BUF_SIZE: usize = 128;
     let mut buf = [0u8; BUF_SIZE];
     // read the filename
     let filename = unsafe {
-        core::str::from_utf8_unchecked(
-            bpf_probe_read_user_str_bytes(
-                filename_addr as *const u8,
-                &mut buf,
-            )?,
-        )
+        core::str::from_utf8_unchecked(bpf_probe_read_user_str_bytes(
+            filename_addr as *const u8,
+            &mut buf,
+        )?)
     };
+    // Attempt to block the execution of the read command
     if filename.contains("read") {
-        return Ok(1);
+        Ok(1);
     }
 
     let pid = bpf_get_current_pid_tgid() as u32;
-    
-    info!(&ctx, "{} {}", pid, filename);
 
+    info!(&ctx, "{} {}", pid, filename);
 
     Ok(0)
 }
